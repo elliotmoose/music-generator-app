a5ba3319ecdcef269f6667ba25dc5137ab95c92e
Sean Lim <seanmichael_lim@mymail.sutd.edu.sg>
2020-12-04 02:22:59 +0800

    added UI, play/stop button, add Chord playing buttons
    relatively laggy for some reason i cannot identify

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import Player from './Player';
 import * as Tone from 'tone'
+import PlayBar from './components/Playbar'
+import data from './public/data'
+
+
 let interval;
 function App() {
+  let SampleChords = data.sampleChords
   const [play, setPlay] = useState(false)
   const [player, setPlayer] = useState(new Player());
+  const [playerTwo, setPlayerTwo] = useState(new Player());
   const [notes, setNotes] = useState([]);
   const [playheadTime, setPlayheadTime] = useState(0);
+  const [presetNotes, setPresetNotes] = useState([])
 
   useEffect(() => {
     player.setup();
     setPlayer(player);
-    
+    playerTwo.setupTwo();
+    setPlayerTwo(playerTwo);
+    setPresetNotes(SampleChords)
+    // console.log("set up")
     if (!interval) {
       setInterval(() => {
         setPlayheadTime(Tone.context.currentTime * DURATION_FACTOR)
       }, 10);
     }
-  }, []);
+  }, [playerTwo,player]);
 
   useEffect(() => {
     if (play) {
-
+      player.setup();
+      setPlayer(player);
+      console.log("set up")
+      console.log("prepping notes");
       (async () => {
         setNotes(await player.notesFromMidiFile('ABeautifulFriendship.mid'));
         await player.playMidiFile('ABeautifulFriendship.mid');
@@ -30,19 +43,38 @@ function App() {
 
       setPlay(false);
     }
-  }, [play])
+  }, [play,player])
 
-  const MAX_MIDI = 91
-  const NOTE_HEIGHT = 20
+  const MAX_MIDI = 88
+  const NOTE_HEIGHT = 8
   const DURATION_FACTOR = 100
+
+  async function handleEvent(e){
+    await playerTwo.playChord(e.target.value);
+  }
+
+  async function handleKeyDown(e){
+    console.log(e.key);
+    if(e.key === e.target.id){
+      console.log(e.target.name)
+      await playerTwo.playChord(e.target.value);
+    }
+  }
 
 
 
   return (
     <div className="App">
-      <button onClick={() => setPlay(true)}>begin</button>
+      <div className="App-header">
+          <div style={{width:"20%",textTransform:"uppercase"}}>2.5K only<br></br> music generation <br></br> project</div>
+          <PlayBar onClickPlay={() => setPlay(true)} onClickPause={() => {setPlay(false)}} onClickStop={async ()=>{await player.stopMidiFile();setNotes([]);setPlay(false);}}></PlayBar>
+          <div style={{width:"20%"}}></div>
+
+      </div>
+      {/* <button onClick={() => setPlay(true)}>begin</button>    */}
+
       {/* <div style={{height: '100%', width: 4, position: 'absolute', left: playheadTime, top: 0, backgroundColor: 'black', transition: '0.2s', zIndex: 999}}/> */}
-      {notes.map((note, i) => {
+      {/* {notes.map((note, i) => {
         // const noteDescription = `${note.name} note: ${note.midi} dur:${note.duration} time:${note.time}`
         const noteDescription = `${note.name}`
         return <div
@@ -50,7 +82,27 @@ function App() {
           style={{ position: 'absolute', left: note.time * DURATION_FACTOR - playheadTime, top: MAX_MIDI * NOTE_HEIGHT - note.midi * NOTE_HEIGHT, width: note.duration * DURATION_FACTOR, height: NOTE_HEIGHT, backgroundColor: 'tomato' }}
         >{noteDescription}</div>
       }
-      )}
+      )} */}
+      <div className="App-piano">
+        {notes.map((note, i) => {
+          // const noteDescription = `${note.name} note: ${note.midi} dur:${note.duration} time:${note.time}`
+          const noteDescription = `${note.name}`
+          return <div
+            key={`${i}`}
+            style={{position:"absolute",left: note.time * DURATION_FACTOR - playheadTime, top: MAX_MIDI * NOTE_HEIGHT - note.midi * NOTE_HEIGHT, width: note.duration * DURATION_FACTOR, height: NOTE_HEIGHT, backgroundColor: '#7Ec291' }}
+          ></div>
+        }
+        )}
+      </div>
+      {/* <div style={{backgroundColor:"yellow",position:"relative"}}> HELLO</div> */}
+      <div className="App-preset-container">
+        {presetNotes.map((data)=> {
+          return <button className="App-preset" onClick={handleEvent} value={data.array} id={`${data.key}`} name={`${data.name}`} onKeyDown={handleKeyDown} >
+            <div style={{paddingTop:"2.5vw",fontSize:"1.25vw"}}>{data.name}</div>
+            <div style={{paddingTop:"1.25vw",fontSize:"1vw",color:"#E37B7B"}}>{data.key}</div>
+          </button>
+        })}
+      </div>
     </div>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,26 @@
 import * as Tone from 'tone'
 import { Midi } from '@tonejs/midi'
 
-let i = 0
 export default class Player {
     setup() {
+        Tone.start()
+        this.sampler = new Tone.Sampler({
+            urls: {
+                "C1": "C1.mp3",
+                "C2": "C2.mp3",
+                "C3": "C3.mp3",
+                "C4": "C4.mp3",
+                "D#4": "Ds4.mp3",
+                "F#4": "Fs4.mp3",
+                "A4": "A4.mp3",
+                "C5": "C5.mp3",
+                "C6": "C6.mp3",
+            },
+            baseUrl: "https://tonejs.github.io/audio/salamander/",
+        }).toDestination()
+    }
+
+    setupTwo(){
         Tone.start()
         this.sampler = new Tone.Sampler({
             urls: {
@@ -28,7 +45,7 @@ export default class Player {
         let objTime = 0
         for (let track of midi.tracks) {
             for (let note of track.notes) {
-                console.log('add note: ' + note.name)
+                //console.log('add note: ' + note.name)
                 objTime += note.time;
                 note.objTime = objTime;
                 notes.push(note);
@@ -40,11 +57,31 @@ export default class Player {
     }
     async playMidiFile(fileName) {
         let notes = await this.notesFromMidiFile(fileName)
+        console.log(notes[0].name)
+        console.log(notes[1].name)
+        console.log(notes[2].name)
         for (let note of notes) {
             this.sampler.triggerAttackRelease([note.name], note.duration, note.time, note.velocity);
-            console.log('play note: ' + note.name)
+            // console.log('play note: ' + note.name)
         }
 
     }
+    // async pauseMidiFile(fileName) {
+    //     let notes = []
+    //     for (let note of notes) {
+    //         this.sampler.triggerAttackRelease([note.name], note.duration, note.time, note.velocity);
+    //         console.log('play note: ' + note.name)
+    //     }
+
+    // }
+    async stopMidiFile() {
+        console.log("cut music")
+        await this.sampler.disconnect();
+        //await this.sampler.dispose();
+    }
     
+    async playChord(notes){
+        // console.log(notes); 
+        this.sampler.triggerAttackRelease(notes,2);
+    }
 }
