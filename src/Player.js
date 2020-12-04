13e85cb56e088ac3b59d37074324de235dc54f35
Elliot Koh <appdev.moose@gmail.com>
2020-12-04 12:35:20 +0800

    Added reinit sampler on stop to remove scheduled notes

--- src/Player.js
+++ src/Player.js
@@ -69,10 +69,35 @@ export default class Player {
 
     // }
     async stopMidiFile() {
-        console.log("cut music")
-        // await this.sampler.disconnect();
-        Tone.Transport.stop()
-        // await this.sampler.dispose();
+       return new Promise((resolve, reject) => {
+            console.log("cut music")
+            Tone.Transport.stop()
+            
+            this.sampler.disconnect();
+            this.sampler.dispose();
+
+            this.sampler = new Tone.Sampler({
+                urls: {
+                    "C1": "C1.mp3",
+                    "C2": "C2.mp3",
+                    "C3": "C3.mp3",
+                    "C4": "C4.mp3",
+                    "D#4": "Ds4.mp3",
+                    "F#4": "Fs4.mp3",
+                    "A4": "A4.mp3",
+                    "C5": "C5.mp3",
+                    "C6": "C6.mp3",
+                },
+                baseUrl: "https://tonejs.github.io/audio/salamander/",
+                onload: () => {
+                    console.log('Sampler Loaded!');
+                    resolve();
+                }
+            }).toDestination();
+
+            this.sampler.sync()
+       })
+       
     }
     
     async pausePlayback() {
